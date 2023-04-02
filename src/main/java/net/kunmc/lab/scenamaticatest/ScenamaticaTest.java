a7d06d2e9cae15a4cea2f384c09a549571b8a1d5
Peyang <peyang@peya.tokyo>
2023-04-02 22:35:06 +0900

    feat: SC005

--- src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
+++ src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
@@ -1,6 +1,7 @@
 package net.kunmc.lab.scenamaticatest;
 
 import net.kunmc.lab.scenamaticatest.commands.SC002_3_4Handler;
+import net.kunmc.lab.scenamaticatest.events.SC005Handler;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,8 @@ public final class ScenamaticaTest extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getPluginCommand("sc002_3_4").setExecutor(new SC002_3_4Handler());
+
+        Bukkit.getPluginManager().registerEvents(new SC005Handler(), this);
     }
 
     @Override
---
+++ src/main/java/net/kunmc/lab/scenamaticatest/events/SC005Handler.java
@@ -0,0 +1,20 @@
+package net.kunmc.lab.scenamaticatest.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class SC005Handler implements Listener
+{
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e)
+    {
+        e.setDeathMessage("You died!");
+
+        Player p = e.getEntity();
+
+        p.sendMessage("The player died!");
+
+    }
+}
