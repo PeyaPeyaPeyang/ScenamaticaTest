e3b51dd5ee910e7a5b84db783c052a5a6eca194d
Peyang <peyang@peya.tokyo>
2023-04-19 20:54:58 +0900

    feat: SC030

--- src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
+++ src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
@@ -1,7 +1,8 @@
 package net.kunmc.lab.scenamaticatest;
 
 import net.kunmc.lab.scenamaticatest.commands.SC002_3_4Handler;
-import net.kunmc.lab.scenamaticatest.events.SC005Handler;
+import net.kunmc.lab.scenamaticatest.commands.SC030Handler;
+import net.kunmc.lab.scenamaticatest.events.SC011Handler;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,8 +13,9 @@ public final class ScenamaticaTest extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getPluginCommand("sc002_3_4").setExecutor(new SC002_3_4Handler());
+        Bukkit.getPluginCommand("sc030").setExecutor(new SC030Handler(this));
 
-        Bukkit.getPluginManager().registerEvents(new SC005Handler(), this);
+        Bukkit.getPluginManager().registerEvents(new SC011Handler(), this);
     }
 
     @Override
--- src/main/java/net/kunmc/lab/scenamaticatest/events/SC011Handler.java
+++ src/main/java/net/kunmc/lab/scenamaticatest/events/SC011Handler.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-public class SC005Handler implements Listener
+public class SC011Handler implements Listener
 {
     @EventHandler
     public void onDeath(PlayerDeathEvent e)
