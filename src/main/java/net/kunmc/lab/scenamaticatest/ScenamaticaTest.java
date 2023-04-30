9c204cff1d4482666929a444c111fdaaf531be23
Peyang <peyang@peya.tokyo>
2023-04-30 17:00:41 +0900

    Replace commitment

--- src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
+++ src/main/java/net/kunmc/lab/scenamaticatest/ScenamaticaTest.java
@@ -1,25 +1,22 @@
 package net.kunmc.lab.scenamaticatest;
 
-import net.kunmc.lab.scenamaticatest.commands.SC002_3_4Handler;
-import net.kunmc.lab.scenamaticatest.commands.SC030Handler;
-import net.kunmc.lab.scenamaticatest.events.SC011Handler;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ScenamaticaTest extends JavaPlugin {
-
+public final class ScenamaticaTest extends JavaPlugin implements Listener
+{
     @Override
-    @SuppressWarnings("ConstantConditions")
-    public void onEnable() {
-        // Plugin startup logic
-        Bukkit.getPluginCommand("sc002_3_4").setExecutor(new SC002_3_4Handler());
-        Bukkit.getPluginCommand("sc030").setExecutor(new SC030Handler(this));
-
-        Bukkit.getPluginManager().registerEvents(new SC011Handler(), this);
+    public void onEnable()
+    {
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e)
+    {
+        e.getEntity().sendMessage("OOPS! You died!");
     }
 }
